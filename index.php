bf44e167447d1f1a330829ef8ff9df6c0c275af0
DenisShupletsov <shuden99@icloud.com>
2019-07-07 20:04:59 +1100

    added index

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>PHP-CALCULATOR</title>
+ </head>
+ <body>
+
+   <form action="/" method="post">
+
+   <p> <input type="text" name="input" size="10" required> = </input></p> 
+   <p> <input type="submit" value="calculate"></input> </p>
+
+   </form>
+
+ </body>
+</html>
