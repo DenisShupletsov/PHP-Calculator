65ab400379c88372d5fb8ddc4aa815cc21ca43f0
DenisShupletsov <shuden99@icloud.com>
2019-07-08 00:59:11 +1100

    Реализовано сложение 2-х чисел

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+
+$objective  = $_GET["objective"];
+$number = explode("+", $objective);
+
+$return = $number[0] + $number[1];
+
+?>
+
 <html>
  <head>
   <meta charset="utf-8">
@@ -5,9 +14,10 @@
  </head>
  <body>
 
-   <form action="/" method="post">
+   <form action="/" method="get">
 
-   <p> <input type="text" name="input" size="10" required> = </input></p> 
+   <p> <input type="text" name="objective" size="10" required></input></p>
+   <p>= <?php echo $return; ?></p>
    <p> <input type="submit" value="calculate"></input> </p>
 
    </form>
